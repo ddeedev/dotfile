593343839c965d340a52a1adf584a54b53cca7c6
ddeedev <dee.hamdee.084@gmail.com>
2026-01-09 10:37:18 +0700

    chore(nvim): vscode diff to code diff

--- nvim/lua/plugins/code-diff.lua
+++ nvim/lua/plugins/code-diff.lua
@@ -1,6 +1,7 @@
 return {
-  "esmuellert/vscode-diff.nvim",
+  "esmuellert/codediff.nvim",
   dependencies = { "MunifTanjim/nui.nvim" },
+  cmd = "CodeDiff",
   config = function()
     require("vscode-diff").setup({
       -- Highlight configuration
